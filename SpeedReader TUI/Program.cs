0d0a2ffa10d8f3edcbdc8552bc06054c4f0e6f79
Rudolf-Snail <81647773+Rudolf-Snail@users.noreply.github.com>
2025-03-20 13:05:05 +0100

    Initial basic version of the program.

---
+++ SpeedReader TUI/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Speed_reader;
+
+var text = "Hello, fellas. How we doing? We doin'? Doing well? I hope so. I haven't had the best of days myself, so I hope you're faring better.";
+var speedReader = new SpeedReader(300, text);
+speedReader.SpeedReadText();
---
+++ SpeedReader TUI/SpeedReader.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Speed_reader
+{
+    internal class SpeedReader
+    {
+        // Fields
+        private decimal wordsPerMinute = 0;
+        private string? text;
+        private string? currentWord;
+
+        // Properties
+        public decimal WordsPerMinute
+        {
+            get
+            {
+                return wordsPerMinute;
+            }
+            set
+            {
+                wordsPerMinute = value;
+            }
+        }
+
+        public string? Text
+        {
+            get
+            {
+                return text;
+            }
+            set
+            {
+                text = value;
+            }
+        }
+
+        public string? CurrentWord
+        {
+            get
+            {
+                return currentWord;
+            }
+            set
+            {
+                currentWord = value;
+            }
+        }
+
+        // Constructors
+        public SpeedReader(decimal wordsPerMinute, string? text = null)
+        {
+            WordsPerMinute = wordsPerMinute;
+            Text = text;
+        }
+
+        // Methods
+        public string[] ConvertTextToWords()
+        {
+            if (Text is null)
+                return Array.Empty<string>();
+
+            char separator = ' ';
+            string[] words = Text.Split(separator);
+
+            return words;
+        }
+
+        public decimal MilisecondsPerWord()
+        {
+            decimal milisecondsInAMinute = 60 * 1000;
+            decimal milisecondsPerWord = milisecondsInAMinute / WordsPerMinute;
+            
+            return milisecondsPerWord;
+        }
+
+        public void SpeedReadText()
+        {
+            string[] words = ConvertTextToWords();
+            decimal milisecondsPerWord = MilisecondsPerWord();
+
+            foreach (string word in words)
+            {
+                Console.Clear();
+
+                CurrentWord = word;
+                Console.WriteLine(CurrentWord);
+
+                Task.Delay(( int ) milisecondsPerWord).Wait();
+            }
+        }
+
+    }
+}
