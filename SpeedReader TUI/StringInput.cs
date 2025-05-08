5b6d61d07242d8407961d0602bc673bcc8b8dd62
Rudolf-Snail <81647773+Rudolf-Snail@users.noreply.github.com>
2025-05-08 14:57:29 +0200

    Added class StringInput. I refactored some things from the Program class and put them into this class.

---
+++ SpeedReader TUI/StringInput.cs
@@ -0,0 +1,36 @@
+﻿using static SpeedReaderTextUserInterface.Input;
+
+namespace SpeedReaderTextUserInterface
+{
+    internal static class StringInput
+    {
+        // Methods
+        public static string? ReceiveCorrectInputValues(string messageToWriteToConsole, string incorrectDataMessage, 
+                                                        ReadAndProcessInput readAndProcessInput, SuccessCondition successCondition)
+        {
+            string? text;
+            
+            while (true)
+            {
+                Console.Write(messageToWriteToConsole);
+
+                text = readAndProcessInput();
+
+                if (!successCondition(text))
+                    Console.WriteLine(incorrectDataMessage);
+                else
+                    break;
+            }
+
+            return text;
+        }
+
+        // Delegates
+        public delegate bool SuccessCondition(string? parsedValue);
+
+        public static bool IsNullOrEmpty(string? parsedValue)
+        {
+            return string.IsNullOrEmpty(parsedValue);
+        }
+    }
+}
