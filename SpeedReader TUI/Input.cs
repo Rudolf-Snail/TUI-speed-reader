9022e2c37d15c264cf9ed7bb7ad40a67fa488c36
Rudolf-Snail <81647773+Rudolf-Snail@users.noreply.github.com>
2025-05-08 14:56:16 +0200

    Added class Input. I refactored some things from the Program class and put them into this class.

---
+++ SpeedReader TUI/Input.cs
@@ -0,0 +1,38 @@
+﻿namespace SpeedReaderTextUserInterface
+{
+    internal static class Input
+    {
+        // Methods
+        public static string? ToCapital(string? text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return text;
+            else if (text.Length == 1)
+                return text.ToUpper();
+            else
+                return text[..1].ToUpper() + text[1..].ToLower();
+        }
+
+        // Delegates
+        public delegate string? ReadAndProcessInput();
+
+        public static string? JustReadInput()
+        {
+            return Console.ReadLine();
+        }
+
+        public static string? ReadAndCapitalizeInput()
+        {
+            return ToCapital(JustReadInput());
+        }
+
+        // Enums
+        public enum TextOptions
+        {
+            Text,
+            File,
+            Align,
+            Reset
+        }
+    }
+}
