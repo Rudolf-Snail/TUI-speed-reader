a2c1c8bdd95bb6b578f5605e5cc82d53f5011532
Rudolf-Snail <81647773+Rudolf-Snail@users.noreply.github.com>
2025-05-10 13:49:27 +0200

    Refactored almost the whole file. I don't have enough energy, motivation or reason to commit and describe the changes individually.

--- SpeedReader TUI/Program.cs
+++ SpeedReader TUI/Program.cs
@@ -36,57 +36,40 @@ decimal speed = SetReadingSpeed();
 var speedReader = new SpeedReader(speed, text, alignHorizontally, alignVertically);
 speedReader.SpeedReadText();
 
-static void ConfigureAlignmentSettings(out bool isValidOption, out bool alignHorizontally, out bool alignVertically)
+static void ConfigureAlignmentSettings(out bool alignHorizontally, out bool alignVertically)
 {
-    do
-    {
-        Console.Write("Do you wish to align the text horizontally? Type in True for yes and False for no: ");
-        isValidOption = bool.TryParse(Console.ReadLine(), out alignHorizontally);
-    } while (!isValidOption);
-
+    string message1 = "Do you wish to align the text horizontally? Type in True for yes and False for no: ";
+    alignHorizontally = NonStringInput<bool>.ReceiveCorrectInputValues(message1, JustReadInput, NonStringInput<bool>.IsParsedCorrectly);
     ChangeConfiguration("alignHorizontally", alignHorizontally.ToString());
 
-    do
-    {
-        Console.Write("Do you wish to align the text vertically? Type in True for yes and False for no: ");
-        isValidOption = bool.TryParse(Console.ReadLine(), out alignVertically);
-    } while (!isValidOption);
-
+    string message2 = "Do you wish to align the text vertically? Type in True for yes and False for no: ";
+    alignVertically = NonStringInput<bool>.ReceiveCorrectInputValues(message2, JustReadInput, NonStringInput<bool>.IsParsedCorrectly);
     ChangeConfiguration("alignVertically", alignVertically.ToString());
 }
 
 static void ProcessUserInput(out string? text, ref bool alignHorizontally, ref bool alignVertically)
 {
-    bool isValidOption;
-    TextOptions option;
-
-    SelectOption(out isValidOption, out option);
-    ProcessOption(ref isValidOption, ref option, out text, ref alignHorizontally, ref alignVertically);
+    SelectOption(out TextOptions option);
+    ProcessOption(ref option, out text, ref alignHorizontally, ref alignVertically);
 }
 
-static void SelectOption(out bool isValidOption, out TextOptions option)
+static void SelectOption(out TextOptions option)
 {
-    while (true)
-    {
-        Console.WriteLine("How do you wish to enter the text to speedread?");
+    string message1 = "How do you wish to enter the text to speedread?\n";
+    string options = "Options:\n";
+    // TODO: Make this dynamic and not static 
+    string text = "Type in Text, if you wish to read text from the command line.\n";
+    string file = "Type in File, if you wish to read text from a file.\n";
+    string align = "Type in Align, if you wish to change the alignment of the text.\n";
+    string reset = "Type in Reset, if you wish to reset the alignment settings to the default values.\n";
+    string choice = "Type in the name of the option you wish to choose: ";
 
-        Console.WriteLine("Options:");
-        Console.WriteLine("Type in Text, if you wish to read text from the command line.");
-        Console.WriteLine("Type in File, if you wish to read text from a file.");
-        Console.WriteLine("Type in Align, if you wish to change the alignment of the text.");
-        Console.WriteLine("Type in Reset, if you wish to reset the alignment settings to the default values.");
+    string[] messages = [message1, options, text, file, align, reset, choice];
 
-        Console.Write("Type in the name of the option you wish to choose: ");
-        isValidOption = Enum.TryParse<TextOptions>(ToCapital(Console.ReadLine()), out option);
-
-        if (!isValidOption)
-            Console.WriteLine("This option is not correct, please try again.");
-        else
-            break;
-    }
+    option = NonStringInput<TextOptions>.ReceiveCorrectInputValues(messages, ReadAndCapitalizeInput, NonStringInput<TextOptions>.IsParsedCorrectly); 
 }
 
-static void ProcessOption(ref bool isValidOption, ref TextOptions option, out string? text, ref bool alignHorizontally, ref bool alignVertically)
+static void ProcessOption(ref TextOptions option, out string? text, ref bool alignHorizontally, ref bool alignVertically)
 {
     switch (option)
     {
@@ -97,50 +80,38 @@ static void ProcessOption(ref bool isValidOption, ref TextOptions option, out st
             text = FileUserInput();
             break;
         case TextOptions.Align:
-            ConfigureAlignmentSettings(out isValidOption, out alignHorizontally, out alignVertically);
+            ConfigureAlignmentSettings(out alignHorizontally, out alignVertically);
             ProcessUserInput(out text, ref alignHorizontally, ref alignVertically);
             break;
         case TextOptions.Reset:
             ChangeConfiguration("alignHorizontally", "false");
+            alignHorizontally = false;
             ChangeConfiguration("alignVertically", "false");
+            alignVertically = false;
             ProcessUserInput(out text, ref alignHorizontally, ref alignVertically);
             break;
         default:
-            throw new ArgumentException($"The specified option — {option.ToString()} — is not valid."); // Should not happen, but it's handled by throwing an error just in case.
+            throw new ArgumentException($"The specified option — {option} — is not valid."); // Should not happen, but it's handled by throwing an error just in case.
     }
 }
 
 static string TextUserInput()
 {
-    string? text;
+    string message = "Enter text for speedreading: ";
 
-    do
-    {
-        Console.Write("Enter text for speedreading: ");
-        text = Console.ReadLine();
-    }
-    while (string.IsNullOrEmpty(text));
-
-    return text;
+    // Should not be a problem as the success condition prevents an empty or null string from being returned. Sadly the code analysis/compiler does not see this. :(
+    #pragma warning disable CS8603 // Possible null reference return.
+    return StringInput.ReceiveCorrectInputValues(message, ReadAndTrimWhitespaces, StringInput.IsNotNullOrEmpty);
+    #pragma warning restore CS8603 // Possible null reference return.
 }
 
 static string FileUserInput()
 {
+    string message = "Enter the file path to the file you wish to speedread: ";
+
+    string? path = StringInput.ReceiveCorrectInputValues(message, ReadAndTrimWhitespaces, StringInput.FileExists);
+
     string text;
-
-    bool fileAvailable;
-    string? path;
-
-    do
-    {
-        Console.Write("Enter the file path to the file you wish to speedread: ");
-        path = Console.ReadLine();
-
-        if (!string.IsNullOrEmpty(path))
-            path = path.Trim('\"');
-
-        fileAvailable = File.Exists(path);
-    } while (!fileAvailable);
 
     // I WANT this to throw an error, if it doesn't find the path, as trying to catch the error and work around it is too difficult and not worth it for me, it's easier to crash the whole program and try again, than to figure out how to solve this issue. 
     #pragma warning disable CS8604 // Possible null reference argument. Disabled becaused FileExists also checks if the string is null and returns false if it is, so it is (or very likely should be) impossible to get through the do-while loop with a null value.
@@ -155,23 +126,15 @@ static string FileUserInput()
 
 static decimal SetReadingSpeed()
 {
-    decimal speed;
-    bool numberEntered;
+    string message = "Enter the speed you wish to read the text at — in Words Per Minute (WPM) — has to be a positive value: ";
 
-    do
-    {
-        Console.Write("Enter the speed you wish to read the text at — in Words Per Minute (WPM) — has to be a positive value: ");
-        numberEntered = decimal.TryParse(Console.ReadLine(), out speed);
-    }
-    while (!numberEntered || speed <= 0);
-
-    return speed;
+    return NonStringInput<decimal>.ReceiveCorrectInputValues(message, JustReadInput, NonStringInput<decimal>.IsPositiveNumber);
 }
 
 static void ChangeConfiguration(string key, string value)
 {
-    var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-    var settings = configFile.AppSettings.Settings;
+    Configuration configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+    KeyValueConfigurationCollection settings = configFile.AppSettings.Settings;
     settings[key].Value = value;
     configFile.Save(ConfigurationSaveMode.Modified);
     ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
