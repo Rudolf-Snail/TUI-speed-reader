3167e477a5b49d53a184f2e3ff17642c713f5354
Rudolf-Snail <81647773+Rudolf-Snail@users.noreply.github.com>
2025-09-24 11:50:11 +0200

    Add GetSpeedOptionConfigurationValueOrDefaultValue method in Program class

--- SpeedReader TUI/Program.cs
+++ SpeedReader TUI/Program.cs
@@ -32,6 +32,8 @@ decimal speed = SetReadingSpeed();
 var speedReader = new SpeedReader(speed, text, alignHorizontally, alignVertically);
 speedReader.SpeedReadText();
 
+SpeedOptions GetSpeedOptionConfigurationValueOrDefaultValue(string settingName) => Enum.TryParse(ConfigurationManager.AppSettings[settingName], out SpeedOptions result) ? result : default;
+
 static void ProcessUserInput(out string? text, ref SpeedOptions speedOption, ref bool alignHorizontally, ref bool alignVertically)
 {
     SelectOption(out TextOptions textOption);
