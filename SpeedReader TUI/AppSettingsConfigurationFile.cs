1fc4a3a428073bb15b99113bfe4b0c9075d27639
Rudolf-Snail <81647773+Rudolf-Snail@users.noreply.github.com>
2025-09-24 11:10:58 +0200

    Add ConfigureSpeedOptionSettings, refactor ConfigureAlignmentSettings and add display current value of text alignment

--- SpeedReader TUI/AppSettingsConfigurationFile.cs
+++ SpeedReader TUI/AppSettingsConfigurationFile.cs
@@ -6,15 +6,37 @@ namespace SpeedReaderTextUserInterface
 {
     internal static class AppSettingsConfigurationFile
     {
-        public static void ConfigureAlignmentSettings(out bool alignHorizontally, out bool alignVertically)
+        public static void ConfigureSpeedOptionSettings(ref SpeedOptions speedOption)
         {
             ConfigurationFileAppSettings(out Configuration configFile, out KeyValueConfigurationCollection settings);
 
-            string message1 = "Do you wish to align the text horizontally? Type in True for yes and False for no: ";
+            string message1 = "What speed option do you wish to use?\n";
+            string options = "Options:\n";
+            // TODO: Make this dynamic and not static 
+            string wordsPerSecond = $"Type in WordsPerSecond or {(int)SpeedOptions.WordsPerSecond}, if you wish to set the amount of words per second the speed reader will display.\n";
+            string wordsPerMinute = $"Type in WordsPerMinute or {(int)SpeedOptions.WordsPerMinute}, if you wish to set the amount of words per minute the speed reader will display.\n";
+            string secondsPerText = $"Type in SecondsPerText or {(int)SpeedOptions.SecondsPerText}, if you wish to set the the amount of seconds the speed reader will display the text in.\n";
+            string minutesPerText = $"Type in MinutesPerText or {(int)SpeedOptions.MinutesPerText}, if you wish to set the the amount of minutes the speed reader will display the text in.\n";
+            string choice = $"Type in the name or number of the option you wish to choose — current option is {speedOption}: ";
+
+            string[] messages = [message1, options, wordsPerSecond, wordsPerMinute, secondsPerText, minutesPerText, choice];
+
+            speedOption = NonStringInput<SpeedOptions>.ReceiveCorrectInputValues(messages, ReadAndCapitalizeInputAndPreserveCase, NonStringInput<SpeedOptions>.IsParsedCorrectly);
+            ChangeSetting("speedOption", speedOption.ToString(), settings);
+
+            SaveSettings(configFile);
+            ReloadSettings(configFile);
+        }
+
+        public static void ConfigureAlignmentSettings(ref bool alignHorizontally, ref bool alignVertically)
+        {
+            ConfigurationFileAppSettings(out Configuration configFile, out KeyValueConfigurationCollection settings);
+
+            string message1 = $"Do you wish to align the text horizontally? Type in True for yes and False for no — current value is {alignHorizontally}: ";
             alignHorizontally = NonStringInput<bool>.ReceiveCorrectInputValues(message1, JustReadInput, NonStringInput<bool>.IsParsedCorrectly);
             ChangeSetting("alignHorizontally", alignHorizontally.ToString(), settings);
 
-            string message2 = "Do you wish to align the text vertically? Type in True for yes and False for no: ";
+            string message2 = $"Do you wish to align the text vertically? Type in True for yes and False for no — current value is {alignVertically}: ";
             alignVertically = NonStringInput<bool>.ReceiveCorrectInputValues(message2, JustReadInput, NonStringInput<bool>.IsParsedCorrectly);
             ChangeSetting("alignVertically", alignVertically.ToString(), settings);
 
