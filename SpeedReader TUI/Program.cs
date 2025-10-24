86817cde65efa3901f004ee23a341ca16f7ac897
Rudolf-Snail <81647773+Rudolf-Snail@users.noreply.github.com>
2025-10-24 23:46:36 +0200

    Remove commented out tests in Program class

--- SpeedReader TUI/Program.cs
+++ SpeedReader TUI/Program.cs
@@ -7,17 +7,5 @@ Console.Title = "Speed reader";
 Console.InputEncoding = System.Text.Encoding.Unicode;
 Console.OutputEncoding = System.Text.Encoding.Unicode;
 
-/* Tests:
-
-//Enum.TryParse(typeof(TextOptions), "Text", out object? option);
-//TextOptions convertedOption = ( TextOptions ) option;
-
-//NonStringInput<TextOptions>.SuccessCondition successCondition = NonStringInput<TextOptions>.IsParsedCorrectly;
-//NonStringInput<TextOptions>.ReceiveCorrectInputValues("Please enter a TextOption enumerator: ", "That is not correct, please try again.", Input.JustReadInput, successCondition);
-
-//NonStringInput<decimal>.SuccessCondition successCondition = NonStringInput<decimal>.IsPositiveNumber;
-//NonStringInput<decimal>.GetCorrectInputValues("Please enter a positive number: ", "That is not correct, please try again.", Input.JustReadInput, successCondition);
-
-*/
 SpeedReader speedReader = new();
 speedReader.ReadEvaluateProcessLoop();
