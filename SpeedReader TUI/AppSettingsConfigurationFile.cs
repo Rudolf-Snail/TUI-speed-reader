76e0fd5b298118d9d9c32f5dcb273db553687d3a
Rudolf-Snail <81647773+Rudolf-Snail@users.noreply.github.com>
2025-10-31 14:47:28 +0100

    Add ConfigureSetting method in AppSettingsConfigurationFile class

--- SpeedReader TUI/AppSettingsConfigurationFile.cs
+++ SpeedReader TUI/AppSettingsConfigurationFile.cs
@@ -40,12 +40,11 @@ namespace SpeedReaderTextUserInterface
         }
 
         public static void ConfigureExitSettings(SpeedReader speedReader)
+        public static void ConfigureSetting(SpeedReader speedReader, string message, SetPropertyAndChangeSetting setProperty)
         {
             ConfigurationFileAppSettings(out Configuration configFile, out KeyValueConfigurationCollection settings);
 
-            string message = $"Do you wish to exit after speed reading through text? Type in True for yes and False for no — current value is {speedReader.ExitAfterSpeedReading}: ";
-            speedReader.ExitAfterSpeedReading = NonStringInput<bool>.ReceiveCorrectInputValues(message, JustReadInput, NonStringInput<bool>.IsParsedCorrectly);
-            ChangeSetting("exitAfterSpeedReading", speedReader.ExitAfterSpeedReading.ToString(), settings);
+            setProperty(speedReader, settings, message);
 
             SaveSettings(configFile);
             ReloadSettings(configFile);
