9a12d600fbebd94bdbf5daae191a5bd93a658ded
Rudolf-Snail <81647773+Rudolf-Snail@users.noreply.github.com>
2025-10-31 13:24:11 +0100

    Add ReadOption class

--- SpeedReader TUI/ReadOption.cs
+++ SpeedReader TUI/ReadOption.cs
@@ -1,5 +1,20 @@
 namespace SpeedReaderTextUserInterface
 {
+    internal static class ReadOption
+    {
+        public static ReadOptions[] ArrayOfValues() => [ReadOptions.Manual, ReadOptions.Automatic];
+
+        public static string ValueDescriptionText(ReadOptions value, SpeedReader? _ = null)
+        => value switch
+        {
+            ReadOptions.Manual => $"Type in {ReadOptions.Manual} or {(int)ReadOptions.Manual}, if you wish to read text manually.",
+            ReadOptions.Automatic => $"Type in {ReadOptions.Automatic} or {(int)ReadOptions.Automatic}, if you wish to read text automatically.",
+            _ => throw new ArgumentException($"The specified option — {value} — is not valid."),
+        };
+
+        public static string DescribeAllValues() => Option<ReadOptions>.DescribeAllValues(ArrayOfValues(), ValueDescriptionText);
+    }
+
     public enum ReadOptions
     {
         Manual,
