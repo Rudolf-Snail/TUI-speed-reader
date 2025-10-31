939b23d23d20fb1db9b65e0e7b1d32de90a9dd3a
Rudolf-Snail <81647773+Rudolf-Snail@users.noreply.github.com>
2025-10-31 13:21:34 +0100

    Add TextOptions values in ValueDescriptionText method

--- SpeedReader TUI/TextOption.cs
+++ SpeedReader TUI/TextOption.cs
@@ -9,8 +9,10 @@ namespace SpeedReaderTextUserInterface
             {
                 TextOptions.Text => $"Type in {TextOptions.Text} or {(int)TextOptions.Text}, if you wish to read text from the command line.",
                 TextOptions.File => $"Type in {TextOptions.File} or {(int)TextOptions.File}, if you wish to read text from a file.",
-                TextOptions.SpeedOption => $"Type in {TextOptions.SpeedOption} or {(int)TextOptions.SpeedOption}, if you wish to change the mode the speed the speed is using — the current value is {speedReader.CurrentSpeedOption}.",
-                TextOptions.AlignOption => $"Type in {TextOptions.AlignOption} or {(int)TextOptions.AlignOption}, if you wish to change the alignment of the text — the current values are AlignHorizontally: {speedReader.AlignHorizontally}, AlignVertically: {speedReader.AlignVertically}.",
+                TextOptions.ReadOption => $"Type in {TextOptions.ReadOption} or {(int)TextOptions.ReadOption}, if you wish to change the mode of reading — the current value is {speedReader.CurrentReadOption}.",
+                TextOptions.SpeedOption => $"Type in {TextOptions.SpeedOption} or {(int)TextOptions.SpeedOption}, if you wish to change the mode speed is using — the current value is {speedReader.CurrentSpeedOption}.",
+                TextOptions.AlignHorizontallyOption => $"Type in {TextOptions.AlignHorizontallyOption} or {(int)TextOptions.AlignHorizontallyOption}, if you wish to change the horizontal alignment of the text — the current value is {speedReader.CurrentAlignHorizontallyOption}.",
+                TextOptions.AlignVerticallyOption => $"Type in {TextOptions.AlignVerticallyOption} or {(int)TextOptions.AlignVerticallyOption}, if you wish to change the vertical alignment of the text — the current value is {speedReader.CurrentAlignVerticallyOption}.",
                 TextOptions.ExitOption => $"Type in {TextOptions.ExitOption} or {(int)TextOptions.ExitOption}, if you wish to exit this program after speed reading — the current value is {speedReader.ExitAfterSpeedReading}.",
                 TextOptions.Reset => $"Type in {TextOptions.Reset} or {(int)TextOptions.Reset}, if you wish to reset the alignment settings to the default values.",
                 TextOptions.Exit => $"Type in {TextOptions.Exit} or {(int)TextOptions.Exit}, if you wish to exit this program.",
