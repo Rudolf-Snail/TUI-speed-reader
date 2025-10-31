066e3ab353476b8da35155ef010c63e261aa6f18
Rudolf-Snail <81647773+Rudolf-Snail@users.noreply.github.com>
2025-10-31 13:19:36 +0100

    Add DescribeAllValues method in Option class

--- SpeedReader TUI/Option.cs
+++ SpeedReader TUI/Option.cs
@@ -2,6 +2,17 @@ namespace SpeedReaderTextUserInterface
 {
     internal static class Option<T>
     {
+        public static string DescribeAllValues(T[] arrayOfValues, ValueDescriptionText valueDescriptionText, SpeedReader? speedReader = null)
+        {
+            var valuesLength = arrayOfValues.Length;
+            var descriptionOfValues = new string[valuesLength];
+
+            for (int index = 0; index < valuesLength; index++)
+                descriptionOfValues[index] = valueDescriptionText(arrayOfValues[index], speedReader);
+
+            return string.Join("\n", descriptionOfValues);
+        }
+        
         public delegate  string ValueDescriptionText(T value, SpeedReader? speedReader);
     }
 }
