7099f7daf8d6401fb8611ed7d292b3af24580f9b
Rudolf-Snail <81647773+Rudolf-Snail@users.noreply.github.com>
2025-11-21 18:44:54 +0100

    Refactor TextOption class

--- SpeedReader TUI/TextOption.cs
+++ SpeedReader TUI/TextOption.cs
@@ -1,8 +1,12 @@
+using static SpeedReaderTextUserInterface.Option;
+using static SpeedReaderTextUserInterface.TextOptions;
+    
 namespace SpeedReaderTextUserInterface
 {
+
     internal static class TextOption
     {
-        public static TextOptions[] ArrayOfValues() => [TextOptions.Text, TextOptions.File, TextOptions.ReadOption, TextOptions.SpeedOption, TextOptions.AlignHorizontallyOption, TextOptions.AlignVerticallyOption, TextOptions.ExitOption, TextOptions.Reset, TextOptions.Exit];
+        public static TextOptions[] ArrayOfValues() => [Text, TextOptions.File, TextOptions.ReadOption, TextOptions.SpeedOption, AlignHorizontallyOption, AlignVerticallyOption, ExitOption, Reset, Exit];
 
         public static string ValueDescriptionText(TextOptions value, SpeedReader? speedReader)
         {
@@ -12,20 +16,20 @@ namespace SpeedReaderTextUserInterface
 
             return value switch
             {
-                TextOptions.Text => $"Type in {TextOptions.Text} or {(int)TextOptions.Text}, if you wish to read text from the command line.",
-                TextOptions.File => $"Type in {TextOptions.File} or {(int)TextOptions.File}, if you wish to read text from a file.",
-                TextOptions.ReadOption => $"Type in {TextOptions.ReadOption} or {(int)TextOptions.ReadOption}, if you wish to change the mode of reading — the current value is {speedReader.CurrentReadOption}.",
-                TextOptions.SpeedOption => $"Type in {TextOptions.SpeedOption} or {(int)TextOptions.SpeedOption}, if you wish to change the mode speed is using — the current value is {speedReader.CurrentSpeedOption}.",
-                TextOptions.AlignHorizontallyOption => $"Type in {TextOptions.AlignHorizontallyOption} or {(int)TextOptions.AlignHorizontallyOption}, if you wish to change the horizontal alignment of the text — the current value is {speedReader.CurrentAlignHorizontallyOption}.",
-                TextOptions.AlignVerticallyOption => $"Type in {TextOptions.AlignVerticallyOption} or {(int)TextOptions.AlignVerticallyOption}, if you wish to change the vertical alignment of the text — the current value is {speedReader.CurrentAlignVerticallyOption}.",
-                TextOptions.ExitOption => $"Type in {TextOptions.ExitOption} or {(int)TextOptions.ExitOption}, if you wish to exit this program after speed reading — the current value is {speedReader.ExitAfterSpeedReading}.",
-                TextOptions.Reset => $"Type in {TextOptions.Reset} or {(int)TextOptions.Reset}, if you wish to reset the alignment settings to the default values.",
-                TextOptions.Exit => $"Type in {TextOptions.Exit} or {(int)TextOptions.Exit}, if you wish to exit this program.",
+                Text => DescriptionOfOption(Text, "read text from the command line"),
+                TextOptions.File => DescriptionOfOption(TextOptions.File, "read text from a file"),
+                TextOptions.ReadOption => DescriptionOfOption(TextOptions.ReadOption, "change the mode of reading", speedReader.CurrentReadOption.ToString()),
+                TextOptions.SpeedOption => DescriptionOfOption(TextOptions.SpeedOption, "change the mode speed is using", speedReader.CurrentSpeedOption.ToString()),
+                AlignHorizontallyOption => DescriptionOfOption(AlignHorizontallyOption, "change the horizontal alignment of the text", speedReader.CurrentAlignHorizontallyOption.ToString()),
+                AlignVerticallyOption => DescriptionOfOption(AlignVerticallyOption, "change the vertical alignment of the text", speedReader.CurrentAlignVerticallyOption.ToString()),
+                ExitOption => DescriptionOfOption(ExitOption, "exit this program after speed reading", speedReader.ExitAfterSpeedReading.ToString()),
+                Reset => DescriptionOfOption(Reset, "reset the alignment settings to the default values"),
+                Exit => DescriptionOfOption(Exit, "exit this program"),
                 _ => throw new ArgumentException($"The specified option — {value} — is not valid."),
             };
         }
 
-        public static string DescribeAllValues(SpeedReader speedReader) => Option<TextOptions>.DescribeAllValues(ArrayOfValues(), ValueDescriptionText, speedReader);
+        public static string DescribeAllValues(SpeedReader speedReader) => Option.DescribeAllValues(ArrayOfValues(), ValueDescriptionText, speedReader);
     }
 
 
